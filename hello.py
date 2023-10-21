5cda6cac6fb92d27efddd504cf13b1852f6b4aa9
guyDangerus <148605963+guyDangerus@users.noreply.github.com>
2023-10-21 10:12:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 ###########################################################################
-####   this is my first code on github, I am trying to push this inbto github from my local machine
-###############################################################################
+# this is my first code on github, I am trying to push this into github ###
+# from my local machine, but shithub don't allow    #######################
+###########################################################################
 
 for i in range(5):
     print(i, ". ", "hello, Github people!")
