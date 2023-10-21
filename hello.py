9adcfbaf7e52bac1ae9c234ffb0442d2c9aefb97
guyDangerus <148605963+guyDangerus@users.noreply.github.com>
2023-10-21 10:11:04 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+###########################################################################
+####   this is my first code on github, I am trying to push this inbto github from my local machine
+###############################################################################
+
+for i in range(5):
+    print(i, ". ", "hello, Github people!")
