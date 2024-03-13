829d6572cea00c48df7625f5f395766c963b9d29
Pratik Modak <pratikmodak13@gmail.com>
2024-03-13 10:29:11 +0530

    initial commit

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 ###########################################################################
 # this is my first code on github, I am trying to push this into github ###
-# from my local machine, but shithub don't allow    #######################
+# from my local machine, but Github don't allow    ########################
+# but now I have done via ssh so they can't stop me  ######################
 ###########################################################################
 
 for i in range(5):
