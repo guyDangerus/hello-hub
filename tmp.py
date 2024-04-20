db1c7521dc95d3602de820b82d0be4a48fc92315
guyDangerus <148605963+guyDangerus@users.noreply.github.com>
2024-04-20 23:59:27 +0530

    Update tmp.py to list downside

--- tmp.py
+++ tmp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+""" the only downside of this version is that the numbers are displayed upto 4 decimal places, at the cost of using the counter properly """
 n1 = st.number_input("Enter the first number --", step = 0.0001, value = 0.0000, format = "%5.4f")
 n2 = st.number_input("Enter the second number --", step = 0.0001, value = 0.0000, format = "%5.4f")
 n3 = st.number_input("Enter the third number --", step = 0.0001, value = 0.0000, format = "%5.4f")
